3d5d1dfc68023a7eb07b53edd263afcec4542096
introvenko <volodymyry94@gmail.com>
2024-12-02 15:55:50 +0100

    Change the number in the message

--- frontend/src/components/CalendarAcceptation/CalendarAcceptationPage.jsx
+++ frontend/src/components/CalendarAcceptation/CalendarAcceptationPage.jsx
@@ -7,7 +7,7 @@ export const CalendarAcceptationPage = () => {
             <h1 class="meeting__title">Plan a meeting</h1>
             <p class="meeting__text--big">
                 You've planned a meeting with coach <span class="meeting__highlight">Basile </span>
-                on <span class="meeting__highlight">9</span> December at <span class="meeting__highlight">10:00</span>.
+                on <span class="meeting__highlight">16</span> December at <span class="meeting__highlight">10:00</span>.
             </p>
             <p class="meeting__text">If you have something to add...</p>
             <textarea class="meeting__textarea" name="" id="">Basile, I want to talk with you about our project.</textarea>
