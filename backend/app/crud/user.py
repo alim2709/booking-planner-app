e0e158136fa9c02957757f97310e8df7472b4d69
alim2709 <alimselemetow@gmail.com>
2024-12-02 11:39:14 +0100

    Add crud operations for user in app/crud/user.py file

---
+++ backend/app/crud/user.py
@@ -0,0 +1,48 @@
+from sqlalchemy import select, insert, or_
+from app.database import async_session_maker
+from app.models.user import User
+
+
+class UserDB:
+    def __init__(self, session=async_session_maker):
+        self.session = session
+
+    async def get_user_by_id(self, user_id):
+        async with self.session() as session:
+            query = select(User).where(User.id == user_id)
+            result = await session.execute(query)
+
+            return result.scalar_one_or_none()
+
+    async def get_user_by_email(self, email: str) -> User:
+        async with self.session() as session:
+            query = select(User).where(User.email == email)
+            result = await session.execute(query)
+            return result.scalar_one_or_none()
+
+    async def get_user_by_username(self, username: str) -> User:
+        async with self.session() as session:
+            query = select(User).where(User.username == username)
+            result = await session.execute(query)
+            return result.scalar_one_or_none()
+
+    # async def get_user_by_email_username(self, email: str, username: str) -> User:
+    #     async with self.session() as session:
+    #         query = select(User).where(
+    #             or_(User.email == email, User.username == username)
+    #         )
+    #         result = await session.execute(query)
+    #
+    #         return result.scalar_one_or_none()
+
+    async def user_exists(self, email: str, username: str) -> bool:
+        user = await self.get_user_by_email_username(email, username)
+
+        return bool(user)
+
+    async def create_user(self, **user_data) -> None:
+        async with self.session() as session:
+            query = insert(User).values(**user_data).returning(User)
+            result = await session.execute(query)
+            await session.commit()
+            return result.scalar_one()
