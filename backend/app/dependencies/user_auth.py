d83443451aafe160edc3a589bd21711edcca361c
alim2709 <alimselemetow@gmail.com>
2024-12-09 13:37:43 +0100

    Refactor TokenBearer at app/dependencies/user_auth.py file

--- backend/app/dependencies/user_auth.py
+++ backend/app/dependencies/user_auth.py
@@ -4,6 +4,7 @@ from fastapi import Request, status
 from fastapi.exceptions import HTTPException
 
 from app.utils.authentication import decode_token
+from app.redis import is_token_in_blocklist
 
 
 class TokenBearer(HTTPBearer):
@@ -19,7 +20,20 @@ class TokenBearer(HTTPBearer):
 
         if not self.token_is_valid(token):
             raise HTTPException(
-                status_code=status.HTTP_403_FORBIDDEN, detail="Token expired or invalid"
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail={
+                    "error": "This token is invalid or expired",
+                    "resolution": "Please get a new token",
+                },
+            )
+
+        if await is_token_in_blocklist(token_data["jti"]):
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail={
+                    "error": "This token is invalid or has been revoked",
+                    "resolution": "Please get a new token",
+                },
             )
 
         self.verify_token_data(token_data)
@@ -30,7 +44,7 @@ class TokenBearer(HTTPBearer):
 
         token_data = decode_token(token)
 
-        return True if token_data else False
+        return token_data is not None
 
     def verify_token_data(self, token_data: dict):
         raise NotImplementedError("Please Implement this method")
